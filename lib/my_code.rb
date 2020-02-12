72949afe6a37c9d8c6737ec03322a22158373971
David Wolff <dcwolff218@gmail.com>
2020-02-12 01:16:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,8 +56,7 @@ def reduce_to_all_true (array)
   index = 0 
   while index < array.length do
     #binding.pry
-    if !array[index] 
-      return false
+    return false if !array[index] 
       index += 1
    end
   end
